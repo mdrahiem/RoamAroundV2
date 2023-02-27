58a2ace5a4f76bab7f5790174e635ebb82f3167b
CXoXo-io <126208733+CXoXo-io@users.noreply.github.com>
2023-02-27 11:06:09 -0500

    Add files via upload

---
+++ city-data.ts
@@ -0,0 +1,386 @@
+export const data = {
+  ['airlie beach']: {
+    link: "https://www.viator.com/Airlie-Beach/d28628-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+  },
+  anchorage: {
+    link: "https://www.viator.com/Anchorage/d4152-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+  },
+  atlanta: {
+    link: "https://www.viator.com/Atlanta/d784-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+  },
+  austin: {
+    link: "https://www.viator.com/Austin/d5021-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+  },
+  baltimore: {
+    link: "https://www.viator.com/Baltimore/d4375-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+  },
+  barcelona: {
+    link: "https://www.viator.com/Barcelona/d562-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+  },
+  ['big island of hawaii']: {
+    link: "https://www.viator.com/Big-Island-of-Hawaii/d669-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+  },
+  boston: {
+    link: "https://www.viator.com/Boston/d678-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+  },
+  broome: {
+    link: "https://www.viator.com/Broome/d789-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+  },
+  ['Cairns & the Tropical North']: {
+    link:  "https://www.viator.com/Cairns-and-the-Tropical-North/d754-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+  },
+  cairo: {
+    link:  "https://www.viator.com/Cairo/d782-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+  },
+  cancun: {
+    link: "https://www.viator.com/Cancun/d631-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+  },
+  ['cape town']: {
+    link:
+    "https://www.viator.com/Cape-Town/d318-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+  },
+  charleston: {
+    link: "https://www.viator.com/Charleston/d4384-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+  },
+  chicago: {
+    link: "https://www.viator.com/Chicago/d673-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+  },
+  clearwater: {
+    link: "https://www.viator.com/Clearwater/d22457-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+  },
+  cozumel: {
+    link: "https://www.viator.com/Cozumel/d632-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+  }
+    // Darwin: {
+    // link:
+    // "https://www.viator.com/Darwin/d360-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Denali National Park: {
+    // link:
+    // "https://www.viator.com/Denali-National-Park/d22364-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Denver: {
+    // link:
+    // "https://www.viator.com/Denver/d4837-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Dublin: {
+    // link:
+    // "https://www.viator.com/Dublin/d503-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Dubrovnik: {
+    // link:
+    // "https://www.viator.com/Dubrovnik/d904-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Fairbanks: {
+    // link:
+    // "https://www.viator.com/Fairbanks/d5269-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Fajardo: {
+    // link:
+    // "https://www.viator.com/Fajardo/d23854-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Florence: {
+    // link:
+    // "https://www.viator.com/Florence/d519-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Fort Lauderdale: {
+    // link:
+    // "https://www.viator.com/Fort-Lauderdale/d660-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Grand Canyon National Park: {
+    // link:
+    // "https://www.viator.com/Grand-Canyon-National-Park/d815-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Grand Teton National Park: {
+    // link:
+    // "https://www.viator.com/Grand-Teton-National-Park/d22873-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Hilton Head Island: {
+    // link:
+    // "https://www.viator.com/Hilton-Head-Island/d31480-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Hobart: {
+    // link:
+    // "https://www.viator.com/Hobart/d379-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Istanbul: {
+    // link:
+    // "https://www.viator.com/Istanbul/d585-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Jackson Hole: {
+    // link:
+    // "https://www.viator.com/Jackson-Hole/d5261-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Kauai: {
+    // link:
+    // "https://www.viator.com/Kauai/d670-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Key Largo: {
+    // link:
+    // "https://www.viator.com/Key-Largo/d23475-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Key West: {
+    // link:
+    // "https://www.viator.com/Key-West/d661-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Krakow: {
+    // link:
+    // "https://www.viator.com/Krakow/d529-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // La Fortuna de San Carlos: {
+    // link:
+    // "https://www.viator.com/La-Fortuna-de-San-Carlos/d50401-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Lake Tahoe: {
+    // link:
+    // "https://www.viator.com/Lake-Tahoe/d816-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Las Vegas: {
+    // link:
+    // "https://www.viator.com/Las-Vegas/d684-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Los Angeles: {
+    // link:
+    // "https://www.viator.com/Los-Angeles/d645-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Mallorca: {
+    // link:
+    // "https://www.viator.com/Mallorca/d955-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Marrakech: {
+    // link:
+    // "https://www.viator.com/Marrakech/d5408-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Maui: {
+    // link:
+    // "https://www.viator.com/Maui/d671-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Mexico City: {
+    // link:
+    // "https://www.viator.com/Mexico-City/d628-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Miami: {
+    // link:
+    // "https://www.viator.com/Miami/d662-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Milan: {
+    // link:
+    // "https://www.viator.com/Milan/d512-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Moab: {
+    // link:
+    // "https://www.viator.com/Moab/d5600-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Montego Bay: {
+    // link:
+    // "https://www.viator.com/Montego-Bay/d432-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Mykonos: {
+    // link:
+    // "https://www.viator.com/Mykonos/d958-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Napa & Sonoma: {
+    // link:
+    // "https://www.viator.com/Napa-and-Sonoma/d914-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Naples: {
+    // link:
+    // "https://www.viator.com/Naples/d508-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Nashville: {
+    // link:
+    // "https://www.viator.com/Nashville/d799-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Nassau: {
+    // link:
+    // "https://www.viator.com/Nassau/d420-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // New Orleans: {
+    // link:
+    // "https://www.viator.com/New-Orleans/d675-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // New York City: {
+    // link:
+    // "https://www.viator.com/New-York-City/d687-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Niagara Falls: {
+    // link:
+    // "https://www.viator.com/Niagara-Falls/d23183-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Niagara Falls & around: {
+    // link:
+    // "https://www.viator.com/Niagara-Falls-and-around/d773-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Oahu: {
+    // link:
+    // "https://www.viator.com/Oahu/d672-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Ocho Rios: {
+    // link:
+    // "https://www.viator.com/Ocho-Rios/d434-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Orlando: {
+    // link:
+    // "https://www.viator.com/Orlando/d663-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Page: {
+    // link:
+    // "https://www.viator.com/Page/d23393-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Panama City Beach: {
+    // link:
+    // "https://www.viator.com/Panama-City-Beach/d22828-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Paris: {
+    // link:
+    // "https://www.viator.com/Paris/d479-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Philadelphia: {
+    // link:
+    // "https://www.viator.com/Philadelphia/d906-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Phoenix: {
+    // link:
+    // "https://www.viator.com/Phoenix/d639-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Playa del Carmen: {
+    // link:
+    // "https://www.viator.com/Playa-del-Carmen/d5501-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Port Douglas: {
+    // link:
+    // "https://www.viator.com/Port-Douglas/d787-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Portland: {
+    // link:
+    // "https://www.viator.com/Portland/d4382-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Porto: {
+    // link:
+    // "https://www.viator.com/Porto/d26879-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Puerto Vallarta: {
+    // link:
+    // "https://www.viator.com/Puerto-Vallarta/d630-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Punta Cana: {
+    // link:
+    // "https://www.viator.com/Punta-Cana/d794-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Queenstown: {
+    // link:
+    // "https://www.viator.com/Queenstown/d407-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Reykjavik: {
+    // link:
+    // "https://www.viator.com/Reykjavik/d905-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Salem: {
+    // link:
+    // "https://www.viator.com/Salem/d22414-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // San Antonio: {
+    // link:
+    // "https://www.viator.com/San-Antonio/d910-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // San Diego: {
+    // link:
+    // "https://www.viator.com/San-Diego/d736-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // San Francisco: {
+    // link:
+    // "https://www.viator.com/San-Francisco/d651-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // San Jose: {
+    // link:
+    // "https://www.viator.com/San-Jose/d793-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Santorini: {
+    // link:
+    // "https://www.viator.com/Santorini/d959-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Savannah: {
+    // link:
+    // "https://www.viator.com/Savannah/d5166-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Scottsdale: {
+    // link:
+    // "https://www.viator.com/Scottsdale/d38685-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Seattle: {
+    // link:
+    // "https://www.viator.com/Seattle/d704-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Sedona: {
+    // link:
+    // "https://www.viator.com/Sedona/d750-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Seville: {
+    // link:
+    // "https://www.viator.com/Seville/d556-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Seward: {
+    // link:
+    // "https://www.viator.com/Seward/d4368-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Sorrento: {
+    // link:
+    // "https://www.viator.com/Sorrento/d947-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Split: {
+    // link:
+    // "https://www.viator.com/Split/d4185-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // St Augustine: {
+    // link:
+    // "https://www.viator.com/St-Augustine/d823-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // St Petersburg: {
+    // link:
+    // "https://www.viator.com/St-Petersburg/d908-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Tamarindo: {
+    // link:
+    // "https://www.viator.com/Tamarindo/d24763-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Tenerife: {
+    // link:
+    // "https://www.viator.com/Tenerife/d5404-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Toronto: {
+    // link:
+    // "https://www.viator.com/Toronto/d623-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Tromso: {
+    // link:
+    // "https://www.viator.com/Tromso/d4362-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Tulum: {
+    // link:
+    // "https://www.viator.com/Tulum/d23012-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Vancouver: {
+    // link:
+    // "https://www.viator.com/Vancouver/d616-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Venice: {
+    // link:
+    // "https://www.viator.com/Venice/d522-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Vienna: {
+    // link:
+    // "https://www.viator.com/Vienna/d454-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Washington DC: {
+    // link:
+    // "https://www.viator.com/Washington-DC/d657-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Zion National Park: {
+    // link:
+    // "https://www.viator.com/Zion-National-Park/d5610-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+    // Zurich: {
+    // link:
+    // "https://www.viator.com/Zurich/d577-ttd?pid=P00023220&uid=U00023666&mcid=58086"
+    // }
+}
